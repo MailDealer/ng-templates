be20bd917c6832edff9b97a6931d5494fd231d5d
Константин Иванов <kewogc@gmail.com>
2017-04-12 17:21:47 +0700

    appconfig to Sprockets

--- lib/ng-templates/engine.rb
+++ lib/ng-templates/engine.rb
@@ -31,35 +31,12 @@ module NgTemplates
         require 'sprockets'
         require 'sprockets/engines'
 
-        # if app.config.ng_templates.htmlcompressor
-        #   require 'htmlcompressor/compressor'
-        #   unless app.config.ng_templates.htmlcompressor.is_a? Hash
-        #     app.config.ng_templates.htmlcompressor = {remove_intertag_spaces: true}
-        #   end
-        # end
-        #
-        # # These engines render markup as HTML
-        # app.config.ng_templates.markups.each do |ext|
-        #   # Processed haml/slim templates have a mime-type of text/html.
-        #   # If sprockets sees a `foo.html.haml` it will process the haml
-        #   # and stop, because the haml output is html. Our html engine won't get run.
-        #   mimeless_engine = Class.new(Tilt[ext]) do
-        #     def self.default_mime_type
-        #       nil
-        #     end
-        #   end
-        #
-        #   app.assets.register_engine ".#{ext}", mimeless_engine
-        # end
-
-        app.config.assets.register_mime_type 'text/ng-html', extensions: ['.nghtml']
-        app.config.assets.register_mime_type 'text/ng-haml', extensions: ['.nghaml']
-        app.config.assets.register_transformer 'text/ng-haml', 'application/javascript', NgTemplates::HamlProcessor
-        app.config.assets.register_transformer 'text/ng-html', 'application/javascript', NgTemplates::Processor
+        Sprockets.register_mime_type 'text/ng-html', extensions: ['.nghtml']
+        Sprockets.register_mime_type 'text/ng-haml', extensions: ['.nghaml']
+        Sprockets.register_transformer 'text/ng-haml', 'application/javascript', NgTemplates::HamlProcessor
+        Sprockets.register_transformer 'text/ng-html', 'application/javascript', NgTemplates::Processor
       end
 
-      # Sprockets Cache Busting
-      # If ART's version or settings change, expire and recompile all assets
       app.config.assets.version = [
         app.config.assets.version,
         'ART',
