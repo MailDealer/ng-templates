d2a07ffb767a65e304fceb0918ba8618ef4fd82f
Alexey Yanchenko <your.elkin@gmail.com>
2020-04-27 17:45:39 +0700

    Capability with rails 6

--- lib/ng-templates/haml_processor.rb
+++ lib/ng-templates/haml_processor.rb
@@ -3,20 +3,16 @@ require 'haml'
 
 module NgTemplates
   class HamlProcessor < Processor
-
     include CompactJavaScriptEscape
 
     def render_html(input)
       template = input[:data]
       haml_engine = Haml::Engine.new(template)
-      output = haml_engine.render(ActionView::Base.new)
-      #av = ActionView::Base.new
-      #puts av.inspect
-      #output = av.render inline: template, :type=> :haml
+      templates_lookup = Rails.root.join('app', 'views')
+      output = haml_engine.render(ActionView::Base.with_view_paths(templates_lookup))
       escape_javascript output
     rescue Haml::SyntaxError => ex
       raise Haml::SyntaxError.new("#{input[:filename]} #{ex.message}", ex.line)
     end
-
   end
 end
